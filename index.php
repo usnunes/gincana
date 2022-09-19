06101c752bf3443b19ece31e2e7daaa190b51e5c
usnunes <uashington.nunes@outlook.com>
2022-09-19 12:57:50 -0400

    19-09-2022

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
                                 </div>
                                 <h5 class="text-muted fw-normal mt-0" title="Number of Customers">Pontuação Geral - Todas as Provas</h5>
                                 <h3 class="mt-3 mb-3">340</h3>
-                                <a type="button" href="grupo-oracao.php" class="btn btn-secondary">
+                                <a type="button" href="geral.php" class="btn btn-secondary">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"></path>
                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"></path>
@@ -126,7 +126,7 @@
                                 </div>
                                 <h5 class="text-muted fw-normal mt-0" title="Number of Orders">Participações em Grupo de Oração</h5>
                                 <h3 class="mt-3 mb-3">34</h3>
-                                <a type="button" href="geral.php" class="btn btn-secondary">
+                                <a type="button" href="grupo-oracao.php" class="btn btn-secondary">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"></path>
                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"></path>
