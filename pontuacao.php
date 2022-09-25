529ee88d1b0a7c06bb4c6e010f337795bd372645
usnunes <uashington.nunes@outlook.com>
2022-09-24 22:11:33 -0400

    Atualização Nova

--- pontuacao.php
+++ pontuacao.php
@@ -13,43 +13,43 @@
 
  	// Pontuação - Alimentos
 	$al1 = 2 + 20; 			//Preto - São Bento
-	$al2 = 150 + 195; 			//Roxo - Santo Afonso
+	$al2 = 150 + 195; 		//Roxo - Santo Afonso
 	$al3 = 0 + 0; 			//Azul - São Gabriel
-	$al4 = 48 + 127; 				//Vermelho - Santo Agostinho 
-	$al5 = 110 + 223; 			//Verde - São Rafael
-	$al6 = 18 + 77; 				//Amarelo - São Sebastião
-	$al7 = 0 + 102; 			//Laranja - São Miguel
+	$al4 = 48 + 127; 		//Vermelho - Santo Agostinho 
+	$al5 = 110 + 223; 		//Verde - São Rafael
+	$al6 = 18 + 77; 		//Amarelo - São Sebastião
+	$al7 = 0 + 102; 		//Laranja - São Miguel
 	$al8 = 0 + 70; 			//Branco - São Domingo Sávi
 	
 	//Pontuação - Grito de Guerra
-	$gg1 = 50; 			//Preto - São Bento
-	$gg2 = 50; 			//Roxo - Santo Afonso
-	$gg3 = 50; 			//Azul - São Gabriel
-	$gg4 = 50; 			//Vermelho - Santo Agostinho 
-	$gg5 = 50; 			//Verde - São Rafael
-	$gg6 = 50; 			//Amarelo - São Sebastião
-	$gg7 = 50; 			//Laranja - São Miguel
-	$gg8 = 50; 			//Branco - São Domingo Sávio
+	$gg1 = 50; 				//Preto - São Bento
+	$gg2 = 50; 				//Roxo - Santo Afonso
+	$gg3 = 50; 				//Azul - São Gabriel
+	$gg4 = 50; 				//Vermelho - Santo Agostinho 
+	$gg5 = 50; 				//Verde - São Rafael
+	$gg6 = 50; 				//Amarelo - São Sebastião
+	$gg7 = 50; 				//Laranja - São Miguel
+	$gg8 = 50; 				//Branco - São Domingo Sávio
 	
 	//Pontuação - Bíblias
-	$bb1 = 00; 			//Preto - São Bento
-	$bb2 = 00; 			//Roxo - Santo Afonso
-	$bb3 = 00; 			//Azul - São Gabriel
-	$bb4 = 00; 			//Vermelho - Santo Agostinho 
-	$bb5 = 00; 			//Verde - São Rafael
-	$bb6 = 00; 			//Amarelo - São Sebastião
-	$bb7 = 00; 			//Laranja - São Miguel
-	$bb8 = 00; 			//Branco - São Domingo Sávio
+	$bb1 = 00; 				//Preto - São Bento
+	$bb2 = 00; 				//Roxo - Santo Afonso
+	$bb3 = 00; 				//Azul - São Gabriel
+	$bb4 = 00; 				//Vermelho - Santo Agostinho 
+	$bb5 = 00; 				//Verde - São Rafael
+	$bb6 = 00; 				//Amarelo - São Sebastião
+	$bb7 = 00; 				//Laranja - São Miguel
+	$bb8 = 00; 				//Branco - São Domingo Sávio
 
 	//Pontuação - Jogos Bíblicos
-	$jb1 = 00; 			//Preto - São Bento
-	$jb2 = 00; 			//Roxo - Santo Afonso
-	$jb3 = 00; 			//Azul - São Gabriel
-	$jb4 = 00; 			//Vermelho - Santo Agostinho 
-	$jb5 = 00; 			//Verde - São Rafael
-	$jb6 = 00; 			//Amarelo - São Sebastião
-	$jb7 = 00; 			//Laranja - São Miguel
-	$jb8 = 00; 			//Branco - São Domingo Sávio
+	$jb1 = 00; 				//Preto - São Bento
+	$jb2 = 00; 				//Roxo - Santo Afonso
+	$jb3 = 00; 				//Azul - São Gabriel
+	$jb4 = 00; 				//Vermelho - Santo Agostinho 
+	$jb5 = 00; 				//Verde - São Rafael
+	$jb6 = 00; 				//Amarelo - São Sebastião
+	$jb7 = 00; 				//Laranja - São Miguel
+	$jb8 = 00; 				//Branco - São Domingo Sávio
 
 	//Pontuação - Geral	
 	$pg1 = $go1 + $al1 + $gg1 + $bb1;		//Preto 	- São Bento
